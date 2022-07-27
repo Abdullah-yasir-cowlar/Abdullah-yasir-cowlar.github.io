68b66bb251637441f1d2fea53cc05e24cf5d11b0
abdullah-yasir-cowlar <abdullah.yasir.cowlar@gmail.com>
2022-07-27 13:17:53 +0500

    changed service worker

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 if (navigator.serviceWorker) {
-  console.log("serviceWorkers are supported");
+    console.log('serviceWorkers are supported')
 
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("../sw_cached_pages.js")
-      .then((reg) => console.log("serverWorker registered"))
-      .catch((err) => console.log("err while registering serviceWorker", err));
-  });
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('../sw_cached_site.js')
+            .then(reg => console.log('serverWorker registered'))
+            .catch(err =>
+                console.log('err while registering serviceWorker', err)
+            )
+    })
 }
